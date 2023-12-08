cae4472c58851c9253a4bbdd38591a7ccefdc212
Dustin Fedie <dlfedie@gmail.com>
2023-12-08 14:01:38 -0600

    second and third challenges

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case '+':
+      return firstNumber + secondNumber;
+    
+    case '-':
+      return firstNumber - secondNumber;
+    
+    case '*':
+      return firstNumber * secondNumber;
+    
+    case '/':
+      return firstNumber / secondNumber;
+    default:
+      throw 'Invalid operator given!';
+  }
+}
 
 module.exports = calculator;
