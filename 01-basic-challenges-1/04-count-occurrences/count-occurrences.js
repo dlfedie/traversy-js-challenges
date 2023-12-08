9fc848e40ca4aacf71b82bafdf1058875c17e076
Dustin Fedie <dlfedie@gmail.com>
2023-12-08 14:11:42 -0600

    lesson 4

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,11 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let occurences = 0;
+  for (let letter of str) {
+    if (letter === char) {
+      occurences++;
+    }
+  }
+  return occurences;
+}
 
 module.exports = countOccurrences;
