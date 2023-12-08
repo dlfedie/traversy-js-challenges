ba5e8a76334f213bec436efcce38c3baf537dbf4
Dustin Fedie <dlfedie@gmail.com>
2023-12-08 16:06:52 -0600

    lesson 9

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,20 @@
-function countVowels() {}
+function countVowels(str) {
+  const normalizedInArray = str.toLowerCase().split('');
+  let count = 0;
+
+  for (let char of normalizedInArray) {
+    if (
+      char === 'a' ||
+      char === 'e' ||
+      char === 'i' ||
+      char === 'o' ||
+      char === 'u'
+    ) {
+        count++;
+      }
+  }
+
+  return count;
+}
 
 module.exports = countVowels;
