2929be767968ad8616572099694a342514162923
Dustin Fedie <dlfedie@gmail.com>
2023-12-08 14:19:01 -0600

    lesson 4 with split

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,12 +1,19 @@
-function countOccurrences(str, char) {
-  let occurences = 0;
-  for (let letter of str) {
-    if (letter === char) {
-      occurences++;
-    }
-  }
-  
-  return occurences;
+// function countOccurrences(str, char) {
+//   let occurences = 0;
+//   for (let letter of str) {
+//     if (letter === char) {
+//       occurences++;
+//     }
+//   }
+
+//   return occurences;
+// }
+
+const countOccurrences = (str, char) => {
+  // console.log(str.split(char));
+  // console.log(str.split(char).length - 1);
+
+  return str.split(char).length - 1;
 }
 
 module.exports = countOccurrences;
