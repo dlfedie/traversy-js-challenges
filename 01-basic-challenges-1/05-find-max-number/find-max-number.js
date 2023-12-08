81ff1e8028622413feaa07ac09f5b2b44edf5808
Dustin Fedie <dlfedie@gmail.com>
2023-12-08 14:51:39 -0600

    lesson 5, but with a for loop, ugh

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,5 +1,19 @@
-function findMaxNumber(arr) {
-  return Math.max(...arr);
-}
+// function findMaxNumber(arr) {
+  // return Math.max(arr);
+// }
 
+function findMaxNumber(arr) {
+  let max;
+  for (let num of arr) {
+    if (max === undefined) {
+      max = num;
+    } else {
+      if (num > max) {
+        max = num;
+      }
+    }
+  }
+  
+  return max;
+}
 module.exports = findMaxNumber;
