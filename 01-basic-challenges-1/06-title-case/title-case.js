13f6ffc3dc14c79d390f4b7d70048156b3650190
Dustin Fedie <dlfedie@gmail.com>
2023-12-08 15:16:33 -0600

    lesson 6..

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,13 @@
-function titleCase() {}
+function titleCase(str) {
+  // normalize the input
+  let words = str.toLowerCase().split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    // console.log(words[i]);
+  }
+
+  return words.join(' ');
+}
 
 module.exports = titleCase;
