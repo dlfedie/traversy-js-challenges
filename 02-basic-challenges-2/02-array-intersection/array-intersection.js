84ef7ade9998c018b89dff3afd3f08df4346f86e
Dustin Fedie <dlfedie@gmail.com>
2024-02-06 15:09:21 -0600

    2-2

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  // const set1 = new Set(arr1)
+  // const set2 = new Set(arr2)
+
+  // const intersection = [new Array(set1)].filter(
+  //   (a) => a.includes([new Array(set2)]))
+  
+  // const intersection = arr1.filter((a) => arr2.includes(a))
+
+  return arr1.filter((a) => arr2.includes(a))
+}
 
 module.exports = arrayIntersection;
