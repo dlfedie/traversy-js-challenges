bb98892df7b37b1df50bab9d08f1f6be59abbf12
Dustin Fedie <dlfedie@gmail.com>
2024-02-06 14:45:56 -0600

    2-1

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,23 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  let outputArray = []
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      outputArray.push('FizzBuzz')
+    }
+    else if (i % 3 === 0) {
+      outputArray.push('Fizz')
+    }
+    else if (i % 5 === 0) {
+      outputArray.push('Buzz')
+    }
+    else {
+      outputArray.push(i)
+    }
+  }
+
+
+  return outputArray
+}
 
 module.exports = fizzBuzzArray;
